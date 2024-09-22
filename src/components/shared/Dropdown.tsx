796749fec40df4c6e686315f6d8fc3efa21a2a29
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2024-09-22 18:49:39 -0500

    added code to IconButton.tsx

--- src/components/shared/Dropdown.tsx
+++
@@ -1,5 +0,0 @@
-// reusable dropdown for priorities
-
-// 1. accept props: 'options', 'selected', 'onChange'
-// 2. render a 'select' element with the 'options' array
-// 3. bind the 'onChange' event to the parent's handler
--- src/components/shared/TextInput.tsx
+++
@@ -1,5 +0,0 @@
-// reusable input field component for text entry (both adding and editing tasks)
-
-// 1. accept props: 'value', 'onChange'
-// 2. render an input element of type 'text'
-// 3. bind the input's 'onChange' event to the parent's handler
